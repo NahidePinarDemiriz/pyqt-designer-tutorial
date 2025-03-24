da9aa4b2c03e13b39570baacefeacdce96fb5e25
NahidePinarDemiriz <demiriznahidepinar@gmail.com>
2025-03-24 12:10:10 +0300

    Files have added.

--- connect1.py
+++ connect1.py
@@ -1,7 +1,7 @@
 from PyQt6 import QtCore, QtGui, QtWidgets
 from connect2 import Ui_SecondWindow
 
-class Ui_Dialog(object):
+class Ui_MainWindow(object):
     def open_window(self):
         # Open second window
         self.window = QtWidgets.QMainWindow()
@@ -14,39 +14,55 @@ class Ui_Dialog(object):
         # Assign thing to second window label
         self.ui.label.setText(thing)
 
-    def setupUi(self, Dialog):
-        Dialog.setObjectName("Dialog")
-        Dialog.resize(304, 306)
-        self.pushButton = QtWidgets.QPushButton(parent=Dialog)
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(304, 306)
+        self.centralwidget = QtWidgets.QWidget(parent=MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.pushButton = QtWidgets.QPushButton(parent=self.centralwidget)
         self.pushButton.clicked.connect(lambda: self.clicker())
         self.pushButton.setGeometry(QtCore.QRect(10, 170, 281, 61))
+        font = QtGui.QFont()
+        font.setPointSize(14)
+        self.pushButton.setFont(font)
         self.pushButton.setObjectName("pushButton")
-        self.pushButton_2 = QtWidgets.QPushButton(parent=Dialog)
+        self.pushButton_2 = QtWidgets.QPushButton(parent=self.centralwidget)
         self.pushButton_2.clicked.connect(lambda: self.open_window())
         self.pushButton_2.setGeometry(QtCore.QRect(10, 240, 281, 61))
+        font = QtGui.QFont()
+        font.setPointSize(14)
+        self.pushButton_2.setFont(font)
         self.pushButton_2.setObjectName("pushButton_2")
-        self.lineEdit = QtWidgets.QLineEdit(parent=Dialog)
+        self.lineEdit = QtWidgets.QLineEdit(parent=self.centralwidget)
         self.lineEdit.setGeometry(QtCore.QRect(10, 20, 281, 141))
         font = QtGui.QFont()
         font.setPointSize(16)
         self.lineEdit.setFont(font)
         self.lineEdit.setObjectName("lineEdit")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(parent=MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 304, 21))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(parent=MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
 
-        self.retranslateUi(Dialog)
-        QtCore.QMetaObject.connectSlotsByName(Dialog)
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
-    def retranslateUi(self, Dialog):
+    def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
-        Dialog.setWindowTitle(_translate("Dialog", "Dialog"))
-        self.pushButton.setText(_translate("Dialog", "Submit"))
-        self.pushButton_2.setText(_translate("Dialog", "Open Window"))
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.pushButton.setText(_translate("MainWindow", "Submit"))
+        self.pushButton_2.setText(_translate("MainWindow", "Open Window"))
 
 
 if __name__ == "__main__":
     import sys
     app = QtWidgets.QApplication(sys.argv)
-    Dialog = QtWidgets.QDialog()
-    ui = Ui_Dialog()
-    ui.setupUi(Dialog)
-    Dialog.show()
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
     sys.exit(app.exec())
